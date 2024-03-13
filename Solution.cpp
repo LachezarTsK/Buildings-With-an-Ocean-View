5d11b63eb9e28b5786f576bbec50f9cb5d95ceae
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-03-13 10:50:41 +0100

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -7,18 +7,18 @@ using namespace std;
 class Solution {
 
 public:
-	vector<int> findBuildings(const vector<int>& heights) const {
-		int currentMaxHeight = 0;
-		vector<int> buildingsWithAnOceanView;
+    vector<int> findBuildings(const vector<int>& heights) const {
+        int currentMaxHeight = 0;
+        vector<int> buildingsWithAnOceanView;
 
-		for (int i = heights.size() - 1; i >= 0; --i) {
-			if (heights[i] > currentMaxHeight) {
-				buildingsWithAnOceanView.push_back(i);
-				currentMaxHeight = heights[i];
-			}
-		}
+        for (int i = heights.size() - 1; i >= 0; --i) {
+            if (heights[i] > currentMaxHeight) {
+                buildingsWithAnOceanView.push_back(i);
+                currentMaxHeight = heights[i];
+            }
+        }
 
-		ranges::reverse(buildingsWithAnOceanView);
-		return buildingsWithAnOceanView;
-	}
+        ranges::reverse(buildingsWithAnOceanView);
+        return buildingsWithAnOceanView;
+    }
 };
